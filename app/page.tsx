2e4fc9555b7122f776f1ef1ddf439ac27a0d3f45
Miguel <miguel.viloria.247@gmail.com>
2023-08-03 23:51:01 -0400

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,49 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 bg-black">
+      <div className="container-flex mx-auto bg-white flex flex-wrap">
+        <div className=" h-32 max-w-6xl p-4 border-4 border-black w-2/3">
+          <h1 className='text-6xl my-4 ml-4'>Miguel Viloria</h1>
+        </div>
+        <div className=" h-32 max-w-lg p-4 border-4 border-black w-1/3">
+          <p className='text-xl my-8 font-semibold ml-4 text-center'>miguel.viloria.247@gmail.com</p>
+        </div>
+        <div className="flex">
+          <div className='w-2/3'>
+            <div className=" h-96 border-4 border-black">
+              <h2 className='text-5xl my-8  ml-4 '>Desarrollador web y editor de video</h2>
+            </div>
+            <div className=" h-32 p-4 border-4 border-black">
+              <h2 className='text-xl my-8  ml-4 '>Desarrollo FullStack</h2>
+            </div>
+            <div className=" h-32 p-4 border-4 border-black ">
+              <h2 className='text-xl my-8  ml-4 '>Edición y producción de video</h2>
+            </div>
+            <div className=" h-32 p-4 border-4 border-black">
+              <h2 className='text-xl my-8  ml-4 '>Pintura</h2>
+            </div>
+          </div>
+          <div className=" h-96  border-4 border-black w-1/3 h-screen">
+            <h2 className='text-5xl my-8  '>Desarrollador web y editor de video</h2>
+          </div>
+        
+        </div>
+        
+        
+      </div>
+      <h3></h3>
+      
+    </main>
+  )
+}
+
+
+
+
+/**
+ * 
+ * <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
@@ -27,7 +68,7 @@ export default function Home() {
           </a>
         </div>
       </div>
-
+      <h1>Holi</h1>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -108,6 +149,5 @@ export default function Home() {
           </p>
         </a>
       </div>
-    </main>
-  )
-}
+ * 
+ */
---
+++ app/webs/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Webs() {
+  return (
+    <div>
+      <h1>webs</h1>
+    </div>
+  )
+}
+
+export default Webs
